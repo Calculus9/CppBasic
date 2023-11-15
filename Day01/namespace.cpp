2b2f370599404ddb1e943ff3c46f9fef1cb814f1
Calculus9 <jingyu-huang@jingyu-huangdeMacBook-Air.local>
2023-11-15 20:45:02 +0800

    feat: namespace初探

---
+++ Day01/namespace.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+// iostream提供了一个叫命名空间的东西，标准的命名空间是std。
+
+
+/*
+没有定义时报错，未声明
+int main(void){
+    cout << "hello" << endl;
+}
+
+*/
--- test.cpp
+++ test.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+// 命名空间，cout属于std中
 using namespace std;
 int main(){
+    // cout就是黑屏幕，流向黑屏幕中。windows和linux的回车不同，\n在linux中可能就不行了，为了统一输出，采用endl来
     cout << "hello world" << endl;
 }
